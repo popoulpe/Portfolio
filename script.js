383bcf4e2c9f98199d5765a7c83266fe224d4034
popoulpe <54855619+popo980@users.noreply.github.com>
2025-01-15 01:07:14 +0100

    transfert pc + code base terminé
    fin du code de base, debut de la phase de redaction du site et aussi d'embélire le site pour toujours et a jamais

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function copyOnClick(elem) {
-    navigator.clipboard.writeText(elem.textContent.value);
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
+const span = document.querySelector("span");
+
+span.onclick = function() {
+  document.execCommand("copy");
+}
+
+span.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", span.textContent);
+    console.log(event.clipboardData.getData("text"))
   }
+});
